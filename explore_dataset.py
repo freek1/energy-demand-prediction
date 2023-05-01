749aa6ed59275f87216909dfa55aebc79a880cc4
Karan <Karan.Chand@ru.nl>
2023-05-01 12:57:31 +0200

    added new necessary files

---
+++ explore_dataset.py
@@ -0,0 +1,39 @@
+# Main imports
+import pandas as pd
+import numpy as np
+
+
+
+weather = pd.read_csv('data/weather.csv')
+pallet_history = pd.read_csv('data/Pallet_history_Gold_Spike.csv')
+inbound = pd.read_csv('data/inbound_loads.csv')
+outbound = pd.read_csv('data/outbound_laods.csv')
+demand = pd.read_csv('data/demand_kWtrain_val.csv')
+
+
+
+weather.info(verbose=True)
+print("\n")
+pallet_history.info(verbose=True)
+print("\n")
+inbound.info(verbose=True)
+print("\n")
+outbound.info(verbose=True)
+print("\n")
+demand.info(verbose=True)
+
+
+# after preprocessing
+preprocessed = True
+
+if preprocessed:
+    train = pd.read_csv('preprocessed_data/train.csv')
+    test = pd.read_csv('preprocessed_data/test.csv')
+    val = pd.read_csv('preprocessed_data/val.csv')
+
+    train.info(verbose=True)
+    print("\n")
+    test.info(verbose=True)
+    print("\n")
+    val.info(verbose=True)
+
